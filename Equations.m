53fb56a19cdab951e35d2f6afd765f3272b7f833
Simon <sapopecki@gmail.com>
2017-03-25 19:46:38 -0400

    test add

--- Equations.m
+++ Equations.m
@@ -2,3 +2,4 @@ x0 = [50,1.6,1,10]; %the initial guess for each of the answers
 oldoptions = optimoptions('fsolve');
 options = optimoptions(oldoptions,'MaxFunctionEvaluations',20000,'MaxIterations',20000);
 [x,fval] = fsolve(@eSolve,x0,options);
+%x is the x values, fval is the evaluated answer
