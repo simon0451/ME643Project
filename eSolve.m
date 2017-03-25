3d90b8f6fbb1b4864c14a639de323679433d1d28
Simon <sapopecki@gmail.com>
2017-03-25 17:19:59 -0400

    Picture of equations

---
+++ eSolve.m
@@ -0,0 +1,12 @@
+%Equation solver - solves systems of non-linear equations
+
+theta = 0; %angle of the AoA bar (radians)
+AoA = 7; %mm, length of member 2
+BoB = 20; %mm, length of member 5
+BC = 100; %mm, length of member 4
+AoBo = 45; %mm, length of member 1 (technically just the length between the two fixed points)
+H = 102; %mm, height of system
+omega = 1; %RPM, rotation of member 2 from motor
+m = .45; %kg, mass of member 6
+k = 175; %N/m, spring constant of spring
+rho = 1070; %kg/m^3, density of the plastic we are using to make the parts from
