2980a020e451aae907c45e452ed8c550f15d05a9
Bell <rmb2005@wildcats.unh.edu>
2017-03-25 15:33:47 -0400

    Sydney's 1st note page and the beginning of Reed's matlab code for the position matrix, to find first four unknowns.

---
+++ positionMAT.m
@@ -0,0 +1,12 @@
+function [theta4, theta5, rx, lac] = positionMAT = (r4, r5, ry, raoa, theta2)
+% Known values of the system
+r4 = ;
+r5 = ;
+ry = ;
+% Coefficient Matrix
+A = [];
+% Variable Matrix
+B = [];
+
+A/B = 
+
