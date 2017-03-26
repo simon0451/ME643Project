ba6b983ec654860e3bfb4a12ba435090c746eab0
Michalak <slm2008@wildcats.unh.edu>
2017-03-25 23:07:56 -0400

    filled coefficient matrices

--- accelerationMAT.m
+++ accelerationMAT.m
@@ -1,11 +1,20 @@
 function [alpha4, alpha5, rx_dprime, lac_dprime] = accelerationMAT = (w4, w5)
 % Known values of the system
-r4 = ;
-r5 = ;
-ry = ;
+r4 = 100; % BC
+r5 = 20;  % BoB
+ry =  57; % H - AoBo
+theta2 = 0:(2*pi)/100:2*pi;
+% AC_t = first derivative of AC
 % Coefficient Matrix
-A = [];
+A = [cos(theta4), -AC*sin(theta4), 0, -1;
+    sin(theta4), AC*cos(theta2), 0, 0;
+    0, -r4*sin(theta4), -r5*sin(theta5), -1;
+    0 -r4*sin(theta4), -r5*sin(theta5), 0];
 % RHS Coefficient Matrix
-B = [];
+B = [AoA*w2^2*cos(theta2) + 2*AC_t*w4*sin(theta4) + AC*w4*(cos(theta4));
+    AoA*w2^2*sin(theta2) - 2*AC_t*w4*cos(theta4) + AC*w4^2*(sin(theta4));
+    w5^2*r5*cos(theta5) + w4^2*r4*cos(theta4);
+    w5^2*r5*sin(theta5) + w4^2*r4*sin(theta4)];
 % Solution Matrix, x
+% x = AC_tt, a4, a5, Rx_tt
 x = A/B;
--- velocityMAT.m
+++ velocityMAT.m
@@ -1,11 +1,20 @@
-function [w4, w5, rx_prime, lac_prime] = velocityMAT = (theta4, theta5)
+function [w4, w5, rx_prime, lac_prime] = velocityMAT = (AC, theta4, theta5,Rx)
 % Known values of the system
-r4 = ;
-r5 = ;
-ry = ;
+r4 = 100; % BC
+r5 = 20;  % BoB
+ry =  57; % H - AoBo
+theta2 = 0:(2*pi)/100:2*pi;
 % Coefficient Matrix
-A = [];
+A = [cos(theta4), -AC*sin(theta4), 0, -1;
+    sin(theta4), AC*cos(theta4), 0, 0;
+    0, -r4*sin(theta4), -r5*sin(theta5), -1
+    0, r4*cos(theta4), r5*cos(theta5), 0];
 % RHS Coefficient Matrix
-B = [];
+B = [AoA*w2*sin(theta2);
+    -AoA*w2*cos(theta2);
+    0;
+    0];
 % Solution Matrix, x
+% x = [AC', w4, w5, Rx']
+
 x = A/B;
